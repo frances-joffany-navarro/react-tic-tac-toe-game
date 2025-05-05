f1ddd58accfb894df14009d0c2980e75a97dd9a7
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2025-05-05 23:59:18 +0200

    adding game history feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Square({ value, onSquareClick }) {
     </button>);
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -56,6 +56,18 @@ export default function Board() {
   )
 }
 
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
