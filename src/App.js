86d3e7ffc3ce1def87f4dc84184bb1123488f51b
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2025-05-13 12:27:32 +0200

    lifting state to parent component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
-export default function Square() {
-  return <button className="square">X</button>;
+const { useState } = require("react");
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>{value}</button>
+  );
+}
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleSquareClick() {
+    setSquares("X");
+  }
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleSquareClick} />
+        <Square value={squares[1]} onSquareClick={handleSquareClick} />
+        <Square value={squares[2]} onSquareClick={handleSquareClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={handleSquareClick} />
+        <Square value={squares[4]} onSquareClick={handleSquareClick} />
+        <Square value={squares[5]} onSquareClick={handleSquareClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={handleSquareClick} />
+        <Square value={squares[7]} onSquareClick={handleSquareClick} />
+        <Square value={squares[8]} onSquareClick={handleSquareClick} />
+      </div>
+    </>
+  );
+
+
+
+
+
 }
