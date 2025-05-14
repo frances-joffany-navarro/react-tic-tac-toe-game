f3c7c06e4536ce862dabfe57cb61ed4ad20f5b1f
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2025-05-15 00:00:09 +0200

    add function

--- src/App.js
+++ src/App.js
@@ -7,25 +7,27 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleSquareClick() {
-    setSquares("X");
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
   }
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleSquareClick} />
-        <Square value={squares[1]} onSquareClick={handleSquareClick} />
-        <Square value={squares[2]} onSquareClick={handleSquareClick} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={handleSquareClick} />
-        <Square value={squares[4]} onSquareClick={handleSquareClick} />
-        <Square value={squares[5]} onSquareClick={handleSquareClick} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={handleSquareClick} />
-        <Square value={squares[7]} onSquareClick={handleSquareClick} />
-        <Square value={squares[8]} onSquareClick={handleSquareClick} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
